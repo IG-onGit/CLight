b570857ea832367a79164fecd171db6e570dcef9
IG-onGit <gziraklirex@gmail.com>
2025-02-07 06:40:21 +0400

    + upload

---
+++ .system/importer.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+__path__ = os.path.dirname(os.path.realpath(__file__))
+sys.pycache_prefix = __path__ + "/__pycache__"
+
+import re
+import json
+import signal
+import shutil
+import base64
+import hashlib
+import pkgutil
+import builtins
+import inquirer
+import datetime
+import sysconfig
+import importlib
+import subprocess
+import winreg as reg
+from colored import fg, bg, attr  # pip
+from cryptography.fernet import Fernet
+from modules.cli import cli
